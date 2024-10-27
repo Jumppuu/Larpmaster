8c12a4df1f7cb58c669e16cc4e6a291040ea04b4
Junior Turunen <p59290@edu.sasky.fi>
2024-10-27 11:39:40 +0200

    Muutoksia backgroundin laittamiseen projektiin..

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,53 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainMenu));
+            copyRightLbl = new Label();
+            titleLbl = new Label();
             SuspendLayout();
+            // 
+            // copyRightLbl
+            // 
+            copyRightLbl.AutoSize = true;
+            copyRightLbl.BackColor = Color.Transparent;
+            copyRightLbl.Font = new Font("Old English Text MT", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            copyRightLbl.ForeColor = Color.Sienna;
+            copyRightLbl.Location = new Point(884, 690);
+            copyRightLbl.Name = "copyRightLbl";
+            copyRightLbl.Size = new Size(266, 28);
+            copyRightLbl.TabIndex = 0;
+            copyRightLbl.Text = "© Rajakatse Fantasia ry";
+            // 
+            // titleLbl
+            // 
+            titleLbl.AutoSize = true;
+            titleLbl.BackColor = Color.Transparent;
+            titleLbl.Font = new Font("Old English Text MT", 104.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            titleLbl.ForeColor = Color.Sienna;
+            titleLbl.Location = new Point(227, 45);
+            titleLbl.Name = "titleLbl";
+            titleLbl.Size = new Size(735, 165);
+            titleLbl.TabIndex = 1;
+            titleLbl.Text = "Larpmaster";
             // 
             // mainMenu
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackgroundImage = Resource1.Larpmasterbackground;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             ClientSize = new Size(1162, 727);
+            Controls.Add(titleLbl);
+            Controls.Add(copyRightLbl);
             Name = "mainMenu";
-            Text = "Form1";
+            Text = "Larpmaster v 0.1";
             Load += mainMenu_Load;
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label copyRightLbl;
+        private Label titleLbl;
     }
 }
